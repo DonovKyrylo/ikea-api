b480940a883a463ef4e3df956a3a7b081b2a6852
Kyryh <kyrykiller07@gmail.com>
2024-05-16 22:50:47 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Product(BaseModel):
+    name: str
+    url: str
+    imageUrl: str
+    price: float
+
+
+@app.get("/")
+def home():
+    return RedirectResponse("/docs")
+
+@app.get("/test")
+def test() -> Product:
+    return Product(
+        name="Test",
+        url="google.com",
+        imageUrl="https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png",
+        price=0
+    )
+
