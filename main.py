41d2744a98908eb97e8f2caa9f943cef494e7fc8
Donov Kyrylo <doonvkyrylo@istitutocorni.it>
2024-05-20 22:11:05 +0200

    Added searchProducts endpoint

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.responses import RedirectResponse
 import uvicorn
 from ikeatypes import *
+import webscraper
+
+def get_enum_query(enum: Enum, default_value: Enum = None) -> Query:
+    return Query(default_value.name if default_value is not None else None, enum=[elem.name for elem in enum])
+
 
 app = FastAPI()
 
@@ -10,6 +15,20 @@ app = FastAPI()
 def home():
     return RedirectResponse("/docs")
 
+@app.get("/searchProducts")
+def search_products(
+        query: str,
+        locale: str = get_enum_query(Locale, Locale.EN_US),
+        color: str = get_enum_query(Color),
+        sort: str = get_enum_query(SortType, SortType.RELEVANCE),
+        includeFeatured: bool = False
+    ):
+    return webscraper.search_request(
+        query,
+        Locale[locale],
+        Color[color] if color else None,
+        SortType[sort],
+        includeFeatured=includeFeatured
     )
 
 def main():
