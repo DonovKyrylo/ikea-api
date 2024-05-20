77d834d1680bc0e777de855e4e5cf6033153a0ac
Donov Kyrylo <doonvkyrylo@istitutocorni.it>
2024-05-20 20:34:17 +0200

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -4,22 +4,49 @@ from ikeatypes import *
 def remove_null_params(params: dict[str]):
     return {key: value for key, value in params.items() if value != None}
 
-def search_request(q: str, **kwargs: str):
-    items = get(
-        f"https://sik.search.blue.cdtapps.com/it/it/search-result-page?q={q}&"
-        + "&".join(f"{key}={value}" for key, value in kwargs.items())
-    ).json()["searchResultPage"]["products"]["main"]["items"]
+def search_request(
+        query: str = None,
+        locale: Locale = Locale.EN_US,
+        color: Color = None,
+        sort: SortType = None,
+        includeFeatured = False
+    ) -> list[Product]:
+
+    request_result = get(
+        f"https://sik.search.blue.cdtapps.com/{locale.value[3:]}/{locale.value[:2]}/search-result-page",
+        remove_null_params({
+            "q": query,
+            "f-colors": color.value if color != None else None,
+            "sort": sort.value if sort != None else None
+        })
+    ).json()
+
+    items = request_result["searchResultPage"]["products"]["main"]["items"]
 
     products: list[Product] = []
     for item in items:
-        product = item.get("product") or item.get("featuredProduct")
+
+        product = item.get("product")
+        isFeatured = False
+
+
+        if includeFeatured and product is None:
+            product = item.get("featuredProduct")
+            isFeatured = True
+
         if product:
             products.append(
                 Product(
                     name=product["name"],
+                    typeName=product["typeName"],
                     url=product["pipUrl"],
                     imageUrl=product["mainImageUrl"],
-                    price=product["salesPrice"]["numeral"]
+                    itemMeasure=product["itemMeasureReferenceText"],
+                    price=ProductPrice(
+                        value=product["salesPrice"]["numeral"],
+                        currencySymbol=product["salesPrice"]["current"]["prefix"]
+                    ),
+                    isFeatured=isFeatured
                 )
             )
 
